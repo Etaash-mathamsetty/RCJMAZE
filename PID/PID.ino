2859af1d071a8861ecac476f5e4595c2a66c0c4f
Alan Jiang <74510449+Oajoeawfjo-Jioa@users.noreply.github.com>
2022-10-09 17:07:12 -0400

    fix tof

--- PID/PID.ino
+++ PID/PID.ino
@@ -15,69 +15,64 @@ VL53L0X tof;
 
 
 void setup() {
-  PI_SERIAL.begin(115200);
-  Serial.begin(115200);
+  //PI_SERIAL.begin(9600);
+  Serial.begin(9600);
   Wire.begin();
-  bno.begin();
+  //bno.begin();
+  Serial.println("starting the code!");
 
-  for (int i = TOF_START; i < TOF_NUMBER; i++){
-     tcaselect(i);
-     tof.setTimeout(500);
-     tof.init();
-     tof.startContinuous();
+  for (int i = TOF_START; i < TOF_NUMBER; i++) {
+    tcaselect(i);
+    tof.setTimeout(500);
+    tof.init();
+    tof.startContinuous();
   }
-  
 }
 
-inline void tcaselect(uint8_t i){
+inline void tcaselect(uint8_t i) {
   if (i > 7)
     return;
 
   Wire.beginTransmission(MUXADDR);
   Wire.write(1 << i);
   Wire.endTransmission();
-  
 }
 
-inline void pi_send_tag(const char* tag)
-{
+inline void pi_send_tag(const char* tag) {
   PI_SERIAL.print(tag);
   PI_SERIAL.print("::");
 }
 
-inline void display_tag(const char* tag)
-{
+inline void display_tag(const char* tag) {
   Serial.print(tag);
   Serial.print(": ");
 }
 
-void display_data (const sensors_event_t& data)
-{
+void display_data(const sensors_event_t& data) {
 
-  switch(data.type)
-  {
+  switch (data.type) {
     case SENSOR_TYPE_ACCELEROMETER:
-    {
-      display_tag("accel");
-      Serial.print("X: ");
-      Serial.print(data.acceleration.x);
-      Serial.print(" Y: ");
-      Serial.print(data.acceleration.y);
-      Serial.print(" Z: ");
-      Serial.println(data.acceleration.z);
-      break;
-    }
+      {
+        display_tag("accel");
+        Serial.print("X: ");
+        Serial.print(data.acceleration.x);
+        Serial.print(" Y: ");
+        Serial.print(data.acceleration.y);
+        Serial.print(" Z: ");
+        Serial.println(data.acceleration.z);
+        break;
+      }
     case SENSOR_TYPE_GYROSCOPE:
-    {
-      display_tag("gyro");
-      Serial.print("X: ");
-      Serial.print(data.gyro.x);
-      Serial.print(" Y: ");
-      Serial.print(data.gyro.y);
-      Serial.print(" Z: ");
-      Serial.println(data.gyro.z);
-      break;
-    }
+      {
+        display_tag("gyro");
+        Serial.print("X: ");
+        Serial.print(data.gyro.x);
+        Serial.print(" Y: ");
+        Serial.print(data.gyro.y);
+        Serial.print(" Z: ");
+        Serial.println(data.gyro.z);
+        break;
+      }
     default:
       break;
   }
@@ -85,68 +80,64 @@ void display_data (const sensors_event_t& data)
 
 
 
-void pi_send_data (const sensors_event_t& data)
-{
-  
-  switch(data.type)
-  {
+void pi_send_data(const sensors_event_t& data) {
+
+  switch (data.type) {
     case SENSOR_TYPE_ACCELEROMETER:
-    {
-      pi_send_tag("accel");
-      PI_SERIAL.print(data.acceleration.x);
-      PI_SERIAL.print(",");
-      PI_SERIAL.print(data.acceleration.y);
-      PI_SERIAL.print(",");
-      PI_SERIAL.println(data.acceleration.z);
-    }
+      {
+        pi_send_tag("accel");
+        PI_SERIAL.print(data.acceleration.x);
+        PI_SERIAL.print(",");
+        PI_SERIAL.print(data.acceleration.y);
+        PI_SERIAL.print(",");
+        PI_SERIAL.println(data.acceleration.z);
+      }
     case SENSOR_TYPE_GYROSCOPE:
-    {
-      pi_send_tag("gyro");
-      PI_SERIAL.print(data.gyro.x);
-      PI_SERIAL.print(",");
-      PI_SERIAL.print(data.gyro.y);
-      PI_SERIAL.print(",");
-      PI_SERIAL.println(data.gyro.z);  
-    }
+      {
+        pi_send_tag("gyro");
+        PI_SERIAL.print(data.gyro.x);
+        PI_SERIAL.print(",");
+        PI_SERIAL.print(data.gyro.y);
+        PI_SERIAL.print(",");
+        PI_SERIAL.println(data.gyro.z);
+      }
     default:
       break;
   }
-
 }
 
-byte get_tof_vals(int threshold){
-  
+byte get_tof_vals(int threshold) {
+
   byte wall = 0;
   int reading;
-  
-  for(int i = TOF_START; i < TOF_NUMBER; i++){
-    
+
+  for (int i = TOF_START; i < TOF_NUMBER; i++) {
+
     tcaselect(i);
     reading = tof.readRangeContinuousMillimeters();
-    Serial.println(reading);
     wall <<= 1;
 
-    if (reading < threshold){
+    if (reading < threshold) {
       wall |= 0b1;
       Serial.println("Wall");
+    } else {
+      Serial.println("No Wall");
     }
-    else{
-      Serial.println("No Wall");  
-    }
+    Serial.println(reading);
   }
-
-  Serial.println("\n\n\n");
 
   return wall;
 }
 
-inline void pi_send_data(byte wall){
-  pi_send_tag("wall");
-  PI_SERIAL.write(wall);
+void send_tof_vals(byte tof_val){
+  pi_send_tag("tof");
+  PI_SERIAL.write(tof_val);
+  PI_SERIAL.println();
 }
 
-void pi_read_data(){
-  while(!PI_SERIAL.available());
+void pi_read_data() {
+  while (!PI_SERIAL.available())
+    ;
 
   String data = PI_SERIAL.readString();
   data.trim();
@@ -154,67 +145,47 @@ void pi_read_data(){
   data += '\n';
   String cur_cmd = "";
   Serial.println(data);
-  for(char c : data)
-  {
-      if(c == 'g' || c == 'f' || c == 't')
-      {
-          if(cur_cmd.length() > 0)
-          {
-              if(cur_cmd[0] == 'g' || cur_cmd[0] == 'f')
-              {
-                  Serial.println("FORWARD");
-              }
-              else
-              {
-                  Serial.println("ERR: Invalid Parameter");
-              }
-          }
-          cur_cmd.remove(0);
-          cur_cmd += c;
+  for (char c : data) {
+    if (c == 'g' || c == 'f' || c == 't') {
+      if (cur_cmd.length() > 0) {
+        if (cur_cmd[0] == 'g' || cur_cmd[0] == 'f') {
+          Serial.println("FORWARD");
+        } else {
+          Serial.println("ERR: Invalid Parameter");
+        }
       }
-      if(c == 'e' || c == 'w' || c == 's' || c == 'n')
-      {
-          if(cur_cmd.length() > 0)
-          {
-              if(cur_cmd[0] == 'f' || cur_cmd[0] == 'g')
-              {
-                    Serial.print("turn to ");
-                    Serial.println(c);
-                    Serial.println("FORWARD");
-              }
-              else if(cur_cmd[0] == 't')
-              {
-                 Serial.print("turn to ");
-                 Serial.println(c);
-              }
-              else
-              {
-                  Serial.println("ERR: Invalid Command");
-              }
-              cur_cmd.remove(0);
-              continue;
-          }
-          else
-          {
-              Serial.println("ERR: Invalid Command");
-              continue;
-          }
+      cur_cmd.remove(0);
+      cur_cmd += c;
+    }
+    if (c == 'e' || c == 'w' || c == 's' || c == 'n') {
+      if (cur_cmd.length() > 0) {
+        if (cur_cmd[0] == 'f' || cur_cmd[0] == 'g') {
+          Serial.print("turn to ");
+          Serial.println(c);
+          Serial.println("FORWARD");
+        } else if (cur_cmd[0] == 't') {
+          Serial.print("turn to ");
+          Serial.println(c);
+        } else {
+          Serial.println("ERR: Invalid Command");
+        }
+        cur_cmd.remove(0);
+        continue;
+      } else {
+        Serial.println("ERR: Invalid Command");
+        continue;
       }
-      if(c == '\n' || c == '\0')
-      {
-          if(cur_cmd.length() > 0)
-          {
-              if(cur_cmd[0] == 'g' || cur_cmd[0] == 'f')
-              {
-                  Serial.println("FORWARD");
-              }
-              else
-              {
-                  Serial.println("ERR: Invalid Parameter");
-              }
-          }
-          cur_cmd.remove(0);
+    }
+    if (c == '\n' || c == '\0') {
+      if (cur_cmd.length() > 0) {
+        if (cur_cmd[0] == 'g' || cur_cmd[0] == 'f') {
+          Serial.println("FORWARD");
+        } else {
+          Serial.println("ERR: Invalid Parameter");
+        }
       }
+      cur_cmd.remove(0);
+    }
   }
 }
 
@@ -233,6 +204,7 @@ void loop() {
   byte test = get_tof_vals(100);
   Serial.print("Tof: ");
   Serial.println(test, BIN);
-
+  //send_tof_vals(test);
+  
   delay(100);
 }
