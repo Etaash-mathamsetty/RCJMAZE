c197de6b0c29adf8a471778646997747d9afd965
Your Name <alawnjr@gmail.com>
2022-10-09 17:09:08 -0400

    add matplotlib stuff

---
+++ PID/pyserial.py
@@ -0,0 +1,22 @@
+import time
+import serial
+
+serial_port = '/dev/ttyS0'
+
+ser = serial.Serial(port = serial_port, baudrate = 115200)
+
+values_dict = {}
+
+while True:
+    
+    data = ser.readline().decode('ascii','ignore').split("::")
+    data[1] = data[1][0:-2]
+    split_data = [float (x) for x in data[1].split(',')]
+    values_dict[data[0]] = split_data
+
+    print(values_dict)
+    
+    ser.write(b'teg\n')
+    
+    
+    #time.sleep(0.1)
