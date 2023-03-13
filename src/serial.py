bb9a649573661bb657758f0798a291270a07cbc2
Etaash Mathamsetty <45927311+Etaash-mathamsetty@users.noreply.github.com>
2023-03-12 21:41:27 -0400

    Fix bug with serial.py
    Make sure at least 5 bytes are present before reading

--- src/serial.py
+++ src/serial.py
@@ -1,10 +1,11 @@
-import Robot as Rb
+#import Robot as Rb
 
 if not simulation:
     # code here
     # keep reading until serial buffer is empty and store to Rb.SetDataValue()
-    data = ser.readline().decode('ascii', 'ignore').split("::")
-    data[1] = data[1][0:-2]
-    split_data = [float (x) for x in data[1].split(',')]
-    Rb.SetDataValue(data[0], split_data)
+    if(ser.in_waiting >= 5):
+        data = ser.readline().decode('ascii', 'ignore').split("::")
+        data[1] = data[1][0:-2]
+        split_data = [float (x) for x in data[1].split(',')]
+        Rb.SetDataValue(data[0], split_data)
 
