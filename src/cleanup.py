7db9285d84baebdac2a1063849c5eca6a70dda94
Etaash Mathamsetty <etaash.mathamsetty@gmail.com>
2023-03-14 19:52:08 -0400

    cleanup.py: release video1

--- src/cleanup.py
+++ src/cleanup.py
@@ -1,7 +1,7 @@
-import cv2
-#variables are defined in vars.py (I did this to avoid multi threading)
+# variables are defined in vars.py (I did this to avoid multi threading)
 
 if not simulation:
     video.release()
+    video1.release()
     
 cv2.destroyAllWindows()
