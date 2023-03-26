9e1ab21498b8645d54a99eb66fc87f0c4fa89f3d
RejectedRyan <98713672+RejectedRyan@users.noreply.github.com>
2023-03-26 13:50:12 -0400

    color fucntion.

--- PID/PID.ino
+++ PID/PID.ino
@@ -276,78 +276,32 @@ void pi_read_data() {
     }
   }
 }
-void print_raw_color(uint16_t r, uint16_t g, uint16_t b, uint16_t c)
-{
-  //log_print("raw color:");
-  Serial.print("R: ");
-  Serial.print(r);
-  Serial.print("\t G: ");
-  Serial.print(g);
-  Serial.print("\t B: ");
-  Serial.print(b);
-  Serial.print("\t C: ");
-  Serial.println(c);
-}
-
-bool black_detect(){
- 
-  uint16_t r,g,b,c;
-  tcs.getRawData(&r,&g, &b, &c);
- // print_raw_color(r,g,b,c);
-  if(c < 900) { 
-    Serial.println("Black square detected"); 
-    return true;
-  }
-  return false;
-}  
-bool blue_detect(){ 
-  uint16_t r,g,b,c; 
-  tcs.getRawData(&r, &g, &b, &c); 
-  if(b < 900 && r < 500 && g < 500){ 
-   Serial.println("Puddle detected: delay 5000");   
-   return true; 
-  }
-  return false; 
-  
-}
-bool silver_detect(){ 
+void returnColor(){
     uint16_t r, g, b, c = 0;  
-    int silver_persistance; 
-    //print_raw_color(r1,g1,b1,c1);
-    tcs.getRawData(&r, &g, &b, &c);
-   
-    //print_raw_color(r2, g2, b2, c2);
-    //Serial.print("qtr[4]: ");
-    //Serial.println(qtr[4]);
-    //print_raw_color(r2,g2,b2,c2);
-  //  Serial.print("r/g:");
-   // Serial.println((r2 / (float)g2) * 10);
-    //Serial.print(" c:");
-    //Serial.print(c2);
-   
-  if (c >=  950 && (r / (float)g) * 10 >= 10.5) {
+    tcs.getRawData(&r, &g, &b, &c); 
+    if (c >=  950 && (r / (float)g) * 10 >= 10.5) {
 
    //   silver_persistance++;
-      Serial.println("Checkpoint detected"); 
-      return true; 
-    
-  } 
-  else  
-      return false; 
-    /*
-    else {
-      silver_persistance = 0;
+      Serial.println("silver detected"); 
+      pi_send_tag("color");
+      PI_SERIAL.println("silver");
+      return; 
     }
-
-    if (silver_persistance >= 2) {
-      utils::stopMotors();
-      Serial.println("Checkpoint detected"); 
-      return;
-    }   
-    */
-} 
-//doesn't actually work just the code for now
-
+    else if(c < 250){ 
+      Serial.println("black detected"); 
+      pi_send_tag("color");
+      PI_SERIAL.println("black");  
+      return;  
+    }
+    else if(b > (r * 2.5)){ 
+      Serial.println("blue detected");  
+      pi_send_tag("color"); 
+      PI_SERIAL.println("blue"); 
+      return; 
+    }
+    else 
+      return;  
+}
 void left(int relative_angle, int speed) {
   motorL.addBoost(TURN_BOOST);
   motorR.addBoost(TURN_BOOST);
