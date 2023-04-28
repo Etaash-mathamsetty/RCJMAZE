c4e5b3b9ac595eb7b7315590095b4b1ecf2ef5fe
Oajoeawfjo Jioa <alawnjr@gmail.com>
2023-04-27 20:14:31 -0400

    add camera test

---
+++ src/camera_test.py
@@ -0,0 +1,27 @@
+import cv2
+
+video = cv2.VideoCapture(0)
+video1 = cv2.VideoCapture(1)
+
+if not video.isOpened():
+	print("failed to open video 0")
+if not video1.isOpened():
+	print("failed to open video 1")
+
+
+while True:
+	frame = video.read()[1]
+	frame1 = video1.read()[1]
+
+	cv2.imshow("frame", frame)
+	cv2.imshow("frame1", frame1)
+
+	key = cv2.waitKey(1) & 0xFF
+
+	if key == ord('q'):
+		break
+
+
+video.release()
+video1.release()
+cv2.destroyAllWindows()
