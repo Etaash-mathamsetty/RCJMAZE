6e1e86bc96cedbc1879ce38fbf32b8ca473ecfe2
Etaash Mathamsetty <etaash.mathamsetty@gmail.com>
2023-04-27 20:19:53 -0400

    fix some formatting

--- src/cv.py
+++ src/cv.py
@@ -8,15 +8,15 @@ def pre_process_img(frame):
     frame = cv2.medianBlur(frame, 5)
     return frame
 
-if(video.isOpened() and video1.isOpened()):
+if video.isOpened() and video1.isOpened():
     frame = cv2.flip(video.read()[1], 1)
     frame2 = cv2.flip(video1.read()[1], 1)
     frame = pre_process_img(frame)
     frame2 = pre_process_img(frame2)
 
 #TODO:
-Rb.SetDataValue("NRK",[0.0])
+Rb.SetDataValue("NRK", [0.0])
 
-if(debug_cv and video.isOpened() and video1.isOpened()):
+if debug_cv and video.isOpened() and video1.isOpened():
     cv2.imshow("frame", frame)
     cv2.imshow("frame2", frame2)
