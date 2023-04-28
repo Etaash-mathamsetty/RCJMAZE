5e784a706e5c5511bccebbbebe0b4ae7f544c30c
Etaash Mathamsetty <45927311+Etaash-mathamsetty@users.noreply.github.com>
2023-04-28 12:13:52 -0400

    fix bugz

--- src/camera_test.py
+++ src/camera_test.py
@@ -11,11 +11,10 @@ if not video1.isOpened():
 while True:
 	if video.isOpened():
 		frame = video.read()[1]
+		cv2.imshow("frame", frame)
 	if video1.isOpened():
 		frame1 = video1.read()[1]
-
-	cv2.imshow("frame", frame)
-	cv2.imshow("frame1", frame1)
+		cv2.imshow("frame1", frame1)
 
 	key = cv2.waitKey(1) & 0xFF
 
