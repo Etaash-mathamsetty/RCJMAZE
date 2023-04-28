5af016133ae8ce0fc1e543dae8dd35a6783cae50
Etaash Mathamsetty <45927311+Etaash-mathamsetty@users.noreply.github.com>
2023-04-28 12:08:13 -0400

    Update camera_test.py

--- src/camera_test.py
+++ src/camera_test.py
@@ -8,10 +8,11 @@ if not video.isOpened():
 if not video1.isOpened():
 	print("failed to open video 1")
 
-
 while True:
-	frame = video.read()[1]
-	frame1 = video1.read()[1]
+	if video.isOpened():
+		frame = video.read()[1]
+	if video1.isOpened():
+		frame1 = video1.read()[1]
 
 	cv2.imshow("frame", frame)
 	cv2.imshow("frame1", frame1)
