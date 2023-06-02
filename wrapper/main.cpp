7123728c58f0e93cbec900370cd86b41c13cb12d
Etaash Mathamsetty <45927311+Etaash-mathamsetty@users.noreply.github.com>
2023-06-02 15:41:05 -0400

    major refactoring of wrapper

--- wrapper/main.cpp
+++ wrapper/main.cpp
@@ -55,14 +55,81 @@ bool has_child_exited(pid_t pid)
     return false;
 }
 
+void run_parent_and_child(const fs::path& path_to_bfs, const fs::path& parent_path)
+{
+    pid_t child_pid = fork();
+    if(child_pid > 0)
+    {
+        std::cout << "parent process starting: " << getpid() << std::endl;
+        //wait for child to start
+        sleep(2);
+        while(!button_pressed())
+        {
+            if(has_child_exited(child_pid))
+            {
+                std::cout << "child process exited (successfully)!" << std::endl;
+                goto natural_exit;
+            }
+        }
+        while(!button_released())
+        {
+            if(has_child_exited(child_pid))
+            {
+                std::cout << "child process exited (successfully)!" << std::endl;
+                goto natural_exit;
+            }
+        }
+#ifdef DEBUG_LOGIC
+        if(has_child_exited(child_pid))
+        {
+            std::cout << "child process exited (successfully)!" << std::endl;
+            goto natural_exit;
+        }
+#endif
+        std::cout << "killing child process: " << child_pid << std::endl;
+        kill(child_pid, SIGKILL);
+    }
+    else
+    {
+        std::cout << "child process starting: " << getpid() << std::endl;
+        int ret = execl(path_to_bfs.c_str(), path_to_bfs.c_str(), NULL);
+        std::cout << "child ret (failed to run): " << ret << std::endl;
+        exit(ret);
+    }
+
+    return;
+
+natural_exit:
+
+    std::cout << "Parent process ending!" << std::endl;
+
+    if(child_pid > 0)
+    {
+        std::string save_path = parent_path.string() + "/save.txt";
+
+        if(fs::exists(save_path))
+        {
+            fs::remove(save_path);
+            std::cout << "removed save.txt" << std::endl;
+        }
+    }
+
+    exit(EXIT_SUCCESS);
+}
+
 //pass in arg to BFS path
 int main(int argc, char **argv)
 {
-    std::string path_to_bfs = "";
+    fs::path path_to_bfs;
     if(argc >= 2)
     {
-        path_to_bfs = std::string(argv[1]);
-        path_to_bfs = fs::canonical(fs::absolute(path_to_bfs)).c_str();
+        std::string path = std::string(argv[1]);
+        if(!fs::exists(path))
+        {
+            std::cout << "pls pass in path that exists" << std::endl;
+            return -1;
+        }
+        path_to_bfs = fs::canonical(fs::absolute(path));
         std::cout << "executing: " << path_to_bfs << std::endl;
     }
     else
@@ -76,73 +143,18 @@ int main(int argc, char **argv)
     pinMode(pin, INPUT);
 #endif
 
-    fs::path parent_path = path_to_bfs;
-    if(!parent_path.has_parent_path())
+    if(!path_to_bfs.has_parent_path())
     {
         std::cout << "no parent path" << std::endl;
         return -1;
     }
-    parent_path = parent_path.parent_path();
-    std::cout << "parent path: " << parent_path.c_str() << std::endl;
+    fs::path parent_path = path_to_bfs.parent_path();
+    std::cout << "parent path: " << parent_path << std::endl;
 
-    std::string parent_path_str = parent_path.c_str();
-
-    pid_t child_pid = 0;
 
     while(true)
     {
-        child_pid = fork();
-        if(child_pid > 0)
-        {
-            std::cout << "parent process starting: " << getpid() << std::endl;
-            while(!button_pressed())
-            {
-                if(has_child_exited(child_pid))
-                {
-                    std::cout << "child process exited (successfully)!" << std::endl;
-                    goto natural_exit;
-                }
-            }
-            while(!button_released())
-            {
-                if(has_child_exited(child_pid))
-                {
-                    std::cout << "child process exited (successfully)!" << std::endl;
-                    goto natural_exit;
-                }
-            }
-#ifdef DEBUG_LOGIC
-            if(has_child_exited(child_pid))
-            {
-                std::cout << "child process exited (successfully)!" << std::endl;
-                goto natural_exit;
-            }
-#endif
-            std::cout << "killing child process: " << child_pid << std::endl;
-            kill(child_pid, SIGKILL);
-        }
-        else
-        {
-            std::cout << "child process starting: " << getpid() << std::endl;
-            int ret = execl(path_to_bfs.c_str(), path_to_bfs.c_str(), NULL);
-            std::cout << "child ret (failed to run): " << ret << std::endl;
-            return ret;
-        }
-    }
-
-natural_exit:
-
-    std::cout << "Parent process ending!" << std::endl;
-
-    if(child_pid > 0)
-    {
-        std::string save_path = parent_path_str + "/save.txt";
-
-        if(fs::exists(save_path))
-        {
-            fs::remove(save_path);
-            std::cout << "removed save.txt" << std::endl;
-        }
+        run_parent_and_child(path_to_bfs, parent_path);
     }
 
     
